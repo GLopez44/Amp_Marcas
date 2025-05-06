742dfa073304522a557fe904b91f59e7d5b7a960
GLopez44 <gonzalopezomg@gmail.com>
2025-05-06 10:53:13 +0200

    5

--- Proyecto_3/main.js
+++ Proyecto_3/main.js
@@ -1,3 +1,6 @@
 localStorage.setItem('nombre','Movil_de_Raul');
 
 alert(localStorage.getItem('nombre'));
+
+let aFrutas = ['kiwi','movil_de_raul','fruta del dragon']; //Esto sale mal no guarda array guarda texto//
+localStorage.setItem('array_frutas',aFrutas);
