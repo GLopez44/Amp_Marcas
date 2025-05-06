570bfc1ba023ea979d9ead889b75bb5607fdad92
GLopez44 <gonzalopezomg@gmail.com>
2025-05-06 11:10:59 +0200

    5

--- Proyecto_4/main.js
+++ Proyecto_4/main.js
@@ -1,5 +1,9 @@
+
 const miBoton = document.getElementById("Bu");
 
 miBoton.addEventListener('click' , () => {
-    
+    let miClave = document.getElementById("Clave").value;
+    let Valor = document.getElementById("Valor").value;
+
+    localStorage.setItem(miClave,miBoton)
 });
